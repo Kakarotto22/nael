3c2932c13c61a43ecd53b07dfa2bd191b2834da8
Karam-DEV <60565100+Karam-DEV@users.noreply.github.com>
2020-05-17 01:22:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,72 @@
 from pyrogram import *
 from time import sleep
+import dataset
 app = Client("short_link")
+
+sudoers = (183924118,1060779034)
+
+db = dataset.connect('sqlite:///data.db')
+table = db['data']
+
+check = table.find_one(id=1)
+if check == None:
+	table.insert(dict(id=1,data=None,cl=0))
 
 
 def leech(client, message):
-	file_name = message.document.file_name
-	msg_id = message.message_id
-	if file_name == "sites.txt":
-		file = message.download(file_name="links.txt")
-		f = open("downloads/links.txt","r")
-		f = f.read()
-		f = f.split("\n")
-		links_number = len(f)
-		for line in f:
-			link = message.reply_text(line, quote=False)
-			msg_id = link.message_id
-			message.reply_text("/leech",reply_to_message_id=msg_id)
-			sleep(60)
+	f = open("downloads/links.txt","r")
+	f = f.read()
+	f = f.split("\n")
+	for line in f:
+		link = message.reply_text(line, quote=False)
+		msg_id = link.message_id
+		message.reply_text("/leech",reply_to_message_id=msg_id)
+		sleep(60)
+
+def forward(client, message):
+	caption = open("downloads/caption.txt","r")
+	caption = caption.read()
+	caption = caption.split("\n")
+	caption_line = table.find_one(id=1)["cl"]
+	print(caption_line)
+	app.send_video(-1001413874309,message.video.file_id,caption=caption[caption_line]+"\nمُشاهدة بجودة متوسّطة 👆👆")
+	table.update(dict(id=1, cl=caption_line+1), ["id"])
 
 
+def main(client,message):
+	try:
+		text = message.text
+	except:
+		pass
+	data = table.find_one(id=1)["data"]
+	if text and text == "download":
+		message.reply_text("Now Send URL files")
+		table.update(dict(id=1, data="URL_file"), ["id"])
+	elif message.document and data == "URL_file":
+		message.download(file_name="links.txt")
+		message.reply_text("Now Send Caption file")
+		table.update(dict(id=1, data="caption_file"), ["id"])
+	elif message.document and data == "caption_file":
+		message.download(file_name="caption.txt")
+		message.reply_text("Ok start Downloading")
+		table.update(dict(id=1, data="None"), ["id"])
+		leech(client,message)
+	if message.text == "/reset":
+		message.reply_text("Caption Line Was reset")
+		table.update(dict(id=1, cl=0), ["id"])
 
 #
-leech_handler = MessageHandler(
-	leech,
-	filters=Filters.document
+main_handler = MessageHandler(
+	main,
+	filters=Filters.document | Filters.text
 )
-app.add_handler(leech_handler)
+app.add_handler(main_handler)
+#
+forward_handler = MessageHandler(
+	forward,
+	filters=Filters.video
+)
+app.add_handler(forward_handler)
 #
 print("Bot Is Running")
 app.run()  # Automatically start() and idle()
