1940d209dfb6762512fb0365e5b7f72b98e4f97a
Karam-DEV <60565100+Karam-DEV@users.noreply.github.com>
2020-05-25 05:53:37 +0300

    Add files via upload

---
+++ db.py
@@ -0,0 +1,141 @@
+import dataset
+import requests
+from time import sleep
+
+db = dataset.connect("sqlite:///data.db")
+table = db['data']
+download_table = db["download_data"]
+
+data = download_table.find_one(id=1)
+if data == None:
+	table.insert(dict(id=1,url=None,channel_id=-1001413874309,main_delay=300,sub_delay=5,data=None,qualty=360))
+
+def info(e_n):
+	data = table.find_one(e_n=e_n)
+	return data
+
+def get_desc(e_n,text=""):
+	if text == "":
+		data = table.find_one(e_n=e_n)
+		return data["desc"]
+	else:
+		table.update(dict(e_n=e_n, desc="{}".format(text)), ["e_n"])
+
+def get_vid(e_n,text=""):
+	if text == "":
+		data = table.find_one(e_n=e_n)
+		return data["vid"]
+	else:
+		table.update(dict(e_n=e_n, vid="{}".format(text)), ["e_n"])
+
+def get_fref(e_n,text=""):
+	if text == "":
+		data = table.find_one(e_n=e_n)
+		return data["file_ref"]
+	else:
+		table.update(dict(e_n=e_n, file_ref="{}".format(text)), ["e_n"])
+
+def add_video(e_n,desc,vid,file_ref):
+	table.insert(dict(e_n=e_n,desc=desc,vid=vid,file_ref=file_ref))
+
+def video_list():
+	data = table.all()
+	videos = []
+	for row in data:
+		videos.append(row["e_n"])
+	return videos
+
+
+def gde(text):
+	text = text.replace("[Blkom.com] ","")
+	text = text.replace(".mp4","")
+	desc = text.replace("Ep","0")
+	e_n = text.replace(" [480p]","")
+	e_n = e_n.replace(" [360p]","")
+	e_n = e_n.replace(".1","")
+	e_n = e_n.split("Ep")
+	e_n = e_n[1]
+	return desc,e_n
+
+def reset_data():
+	table.delete()	
+	
+def get_data(text=""):
+	if text == "":
+		data = download_table.find_one(id=1)
+		return data["data"]
+	else:
+		download_table.update(dict(id=1, data=text), ["id"])
+
+def get_channel_id(text=""):
+	if text == "":
+		data = download_table.find_one(id=1)
+		return data["channel_id"]
+	else:
+		download_table.update(dict(id=1, channel_id=text), ["id"])
+
+def get_main_delay(text=""):
+	if text == "":
+		data = download_table.find_one(id=1)
+		return data["main_delay"]
+	else:
+		download_table.update(dict(id=1, main_delay=text), ["id"])
+
+def get_sub_delay(text=""):
+	if text == "":
+		data = download_table.find_one(id=1)
+		return data["sub_delay"]
+	else:
+		download_table.update(dict(id=1, sub_delay=text), ["id"])
+
+def get_url(text=""):
+	if text == "":
+		data = download_table.find_one(id=1)
+		return data["url"]
+	else:
+		download_table.update(dict(id=1, url=text), ["id"])
+
+def get_qualty(text=""):
+	if text == "":
+		data = download_table.find_one(id=1)
+		return data["qualty"]
+	else:
+		download_table.update(dict(id=1, qualty=text), ["id"])
+
+def get_link(link,qualty):
+	r = requests.get(link).text
+	page = r.split("\n")
+	line = r.split("\n")
+	for element in line:
+		if qualty == 360:
+			if "360p <small>" in element or "360 <small>" in element:
+				line = line.index(element)
+		elif qualty == 480:
+			if "480p <small>" in element or "480 <small>" in element:
+				line = line.index(element)
+		elif qualty == 720:
+			if "720p <small>" in element or "720 <small>" in element:
+				line = line.index(element)
+	line = int(line)-1
+	link = page[line]
+	link = link.replace('<a href="','')
+	link = link.replace('" class="btn btn-default" title="Fansub">','')
+	return link
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,36 +1,17 @@
 from pyrogram import *
 from time import sleep
-import dataset
-app = Client("short_link")
+from db import *
 
-sudoers = (183924118,1060779034)
+app = Client("forward")
 
-db = dataset.connect('sqlite:///data.db')
-table = db['data']
+sudoers = [183924118,1060779034]
 
-check = table.find_one(id=1)
-if check == None:
-	table.insert(dict(id=1,data=None,cl=0))
-
-
-def leech(client, message):
-	f = open("downloads/links.txt","r")
-	f = f.read()
-	f = f.split("\n")
-	for line in f:
-		link = message.reply_text(line, quote=False)
-		msg_id = link.message_id
-		message.reply_text("/leech",reply_to_message_id=msg_id)
-		sleep(60)
-
-def forward(client, message):
-	caption = open("downloads/caption.txt","r")
-	caption = caption.read()
-	caption = caption.split("\n")
-	caption_line = table.find_one(id=1)["cl"]
-	print(caption_line)
-	app.send_video(-1001413874309,message.video.file_id,caption=caption[caption_line]+"\nمُشاهدة بجودة متوسّطة 👆👆")
-	table.update(dict(id=1, cl=caption_line+1), ["id"])
+def forward(client,message):
+	caption = message.caption
+	vid = message.video.file_id
+	file_ref = message.video.file_ref
+	desc , e_n = gde(caption)
+	add_video(e_n,desc,vid,file_ref)
 
 
 def main(client,message):
@@ -38,22 +19,108 @@ def main(client,message):
 		text = message.text
 	except:
 		pass
-	data = table.find_one(id=1)["data"]
-	if text and text == "download" and message.from_user.id in sudoers:
-		message.reply_text("Now Send URL files")
-		table.update(dict(id=1, data="URL_file"), ["id"])
-	elif message.document and data == "URL_file" and message.from_user.id in sudoers:
-		message.download(file_name="links.txt")
-		message.reply_text("Now Send Caption file")
-		table.update(dict(id=1, data="caption_file"), ["id"])
-	elif message.document and data == "caption_file" and message.from_user.id in sudoers:
-		message.download(file_name="caption.txt")
-		message.reply_text("Ok start Downloading")
-		table.update(dict(id=1, data="None"), ["id"])
-		leech(client,message)
-	if message.text == "/reset" and message.from_user.id in sudoers:
-		message.reply_text("Caption Line Was reset")
-		table.update(dict(id=1, cl=0), ["id"])
+	chat_id = message.chat.id
+	user_id = message.from_user.id
+	data = get_data()
+	if text and text =="تحميل" and user_id in sudoers:
+		message.reply_text("ارسل الان الرابط")
+		get_data("URL")
+	elif text and data == "URL" and user_id in sudoers:
+		get_url(text)
+		message.reply_text("ارسل الان عدد الحلقات")
+		get_data("e_n")
+	elif text and data == "e_n" and user_id in sudoers:
+		get_data("None")
+		link = get_url()
+		e_n = text
+		i  = 1
+		qualty = get_qualty()
+		while i < int(e_n)+1:
+			main_delay = get_main_delay()
+			url = get_link("{}{}".format(link,i),qualty)
+			msg = app.send_message(chat_id,"{}".format(url))
+			msg.reply_text("/leech")
+			i += 1
+			if i+3 < int(e_n)+1:
+				sub_delay = get_sub_delay()
+				sleep(int(sub_delay))
+				url = get_link("{}{}".format(link,i),qualty)
+				msg = app.send_message(chat_id,"{}".format(url))
+				msg.reply_text("/leech")
+				i += 1
+				sleep(int(sub_delay))
+				url = get_link("{}{}".format(link,i),qualty)
+				msg = app.send_message(chat_id,"{}".format(url))
+				msg.reply_text("/leech")
+				i += 1
+				sleep(int(sub_delay))
+				url = get_link("{}{}".format(link,i),qualty)
+				msg = app.send_message(chat_id,"{}".format(url))
+				msg.reply_text("/leech")
+				i += 1
+			sleep(int(main_delay))
+		app.send_message(chat_id,"Download Finished")
+	elif text == "ارسال" and user_id in sudoers:
+		videos = video_list()
+		videos = sorted(videos)
+		for video in videos:
+			vid = get_vid(video)
+			desc = get_desc(video)
+			file_ref = get_fref(video)
+			app.send_video(chat_id=int(get_channel_id()),video=vid,file_ref=file_ref,caption=desc+"\nمُشاهدة بجودة متوسّطة 👆👆")
+			sleep(3)
+		message.reply("All videos was send")
+		reset_data()
+	elif text == "review":
+		videos = video_list()
+		print(videos)
+		videos = sorted(videos)
+		msg = "You have {} videos to send:".format(len(videos))
+		for video in videos:
+			desc = get_desc(video)
+			msg += "\n{} {}".format(video,desc)
+		message.reply(msg)
+	elif text == "/qualty":
+		qualty = get_qualty()
+		message.reply("qualty is : {}\nYou can Change it just send /setqualty".format(qualty))
+	elif text == "/channel":
+		channel_id = get_channel_id()
+		message.reply("Main delay is : {}\nYou can Change it just send /setchannel".format(channel_id))
+	elif text == "/maindelay":
+		main_delay = get_main_delay()
+		message.reply("Main delay is : {}\nYou can Change it just send /setmaindelay".format(main_delay))
+	elif text == "/subdelay":
+		sub_delay = get_sub_delay()
+		message.reply("Sub delay is : {}\nYou can Change it just send /setsubdelay".format(sub_delay))
+	elif text == "/setqualty":
+		message.reply("Please Send New qualty\nLike This : \n360")
+		get_data("change_qualty")
+	elif text == "/setchannel":
+		message.reply("Please Send New Channel id\nLike This : \n-10082838028")
+		get_data("change_channel")
+	elif text == "/setmaindelay":
+		message.reply("Please Send New Main Delay\nLike This : \n5 Delay for 5 Second")
+		get_data("change_main_delay")
+	elif text == "/setsubdelay":
+		message.reply("Please Send New Sub Delay\nLike This : \n5 Delay for 5 Second")
+		get_data("change_sub_delay")
+	elif text and data == "change_qualty" and user_id in sudoers:
+		get_data("None")
+		get_qualty(text)
+		message.reply("Qualty Was changed")
+	elif text and data == "change_channel" and user_id in sudoers:
+		get_data("None")
+		get_channel_id(text)
+		message.reply("Channel Was changed")
+	elif text and data == "change_main_delay" and user_id in sudoers:
+		get_data("None")
+		get_main_delay(text)
+		message.reply("Main delay Was changed")
+	elif text and data == "change_sub_delay" and user_id in sudoers:
+		get_data("None")
+		get_sub_delay(text)
+		message.reply("Sub delay Was changed")
+
 
 #
 main_handler = MessageHandler(
@@ -68,5 +135,9 @@ forward_handler = MessageHandler(
 )
 app.add_handler(forward_handler)
 #
-print("Bot Is Running")
+
+
+
+
+
 app.run()  # Automatically start() and idle()
